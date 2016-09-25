d9779bcf4e06dcad9bd4e8f89acf2ec8316c00a5
Coy Sanders <coymeetsworld@gmail.com>
2016-09-25 01:54:27 -0700

    DataVis: ForceDirectedGraph: Minor refactoring. Added some notes to get started.

--- js/forcedirectedgraph.js
+++ js/forcedirectedgraph.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
 	var margin = {top: 20, right: 30, bottom: 30, left: 40}
-	var chartWidth = 1270 - margin.left - margin.right;
-	var chartHeight = 650 - margin.top - margin.bottom;
+	var graphWidth = 1270 - margin.left - margin.right;
+	var graphHeight = 650 - margin.top - margin.bottom;
 
-	var chart = d3.select('#chart')
-								.attr("width", chartWidth+margin.left+margin.right)
-								.attr("height", chartHeight+margin.top+margin.bottom)
+	var graph = d3.select('#graph')
+								.attr("width", graphWidth+margin.left+margin.right)
+								.attr("height", graphHeight+margin.top+margin.bottom)
 								.append("g")
 								.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -19,8 +19,18 @@ $(document).ready(function() {
 		//nodes[x].country = "East Timor"	
 		//nodes[x].code = "tl"
 		console.log(countryData.links);
+		//edges
 		//links[y].target = 66 = countryData.nodes[66];
 		//links[y].source = 0 = countryData.nodes[0];
+
+		var nodes = countryData.nodes;
+		var edges = countryData.links;
+
+		/*var force = d3.forceSimulation()
+									.size([graphWidth, graphHeight])
+									.nodes(nodes)
+									.links(edges);*/
+
 
 	});
 });
