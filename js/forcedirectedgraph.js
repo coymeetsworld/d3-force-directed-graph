dd9fe524d0d12d49da597fe03c4285ad920e7404
Coy Sanders <coymeetsworld@gmail.com>
2016-09-26 18:45:14 -0700

    DataVis: ForceDirectedGraph: Able to render links and nodes to graph and bound them inside a container.

--- js/forcedirectedgraph.js
+++ js/forcedirectedgraph.js
@@ -1,36 +1,81 @@
 $(document).ready(function() {
 
-	var margin = {top: 20, right: 30, bottom: 30, left: 40}
-	var graphWidth = 1270 - margin.left - margin.right;
-	var graphHeight = 650 - margin.top - margin.bottom;
+	var graphWidth = 800;
+	var graphHeight = 800;
+	var radius = 2;
 
 	var graph = d3.select('#graph')
-								.attr("width", graphWidth+margin.left+margin.right)
-								.attr("height", graphHeight+margin.top+margin.bottom)
-								.append("g")
-								.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+								.attr("width", graphWidth)
+								.attr("height", graphHeight)
+								.append("g");
 
+	var simulation = d3.forceSimulation()
+											.force("link", d3.forceLink().distance(15))
+											.force("charge", d3.forceManyBody())
+											.force("center", d3.forceCenter(graphWidth/2, graphHeight/2));
 
 	d3.json('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json', function(error, countryData) {
 
 		if (error) throw error;
 		
-		console.log(countryData.nodes);
-		//nodes[x].country = "East Timor"	
-		//nodes[x].code = "tl"
-		console.log(countryData.links);
-		//edges
-		//links[y].target = 66 = countryData.nodes[66];
-		//links[y].source = 0 = countryData.nodes[0];
+		var link = graph.append("g")
+										.attr("class", "links")
+										.selectAll("line")
+										.data(countryData.links)
+										.enter().append("line")
+										.attr("stroke-width", 2);
+
+		var node = graph.append("g")
+										.attr("class", "nodes")
+										.selectAll("circle")
+										.data(countryData.nodes)
+										.enter().append("circle")
+										.attr("r", 5)
+										.attr("fill", function(d) { return 'red'; })
+										.call(d3.drag()
+														.on("start", dragstarted)
+														.on("drag", dragged)
+														.on("end", dragended));
+
+		/* On hover shows the country name. */
+		node.append("title")
+				.text(function(d) { return d.country; });
+
+		simulation.nodes(countryData.nodes).on("tick", ticked);
+		simulation.force("link").links(countryData.links);
 
 		var nodes = countryData.nodes;
 		var edges = countryData.links;
 
-		/*var force = d3.forceSimulation()
-									.size([graphWidth, graphHeight])
-									.nodes(nodes)
-									.links(edges);*/
+		function ticked() {
+			link.attr("x1", function(d) { return d.source.x; })
+					.attr("y1", function(d) { return d.source.y; })
+					.attr("x2", function(d) { return d.target.x; })
+					.attr("y2", function(d) { return d.target.y; });
 
+			/* keeps nodes bound within the box. */
+			node.attr("cx", function(d) { return d.x = Math.max(radius, Math.min(graphWidth - radius, d.x)); })
+					.attr("cy", function(d) { return d.y = Math.max(radius, Math.min(graphHeight - radius, d.y)); });
+		}
 
 	});
+
+	function dragstarted(d) {
+		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+		d.fx = d.x;
+		d.fy = d.y;
+	}
+
+	function dragged(d) {
+		d.fx = d3.event.x;
+		d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+		if (!d3.event.active) simulation.alphaTarget(0);
+		d.fx = null;
+		d.fy = null;
+	}
+
+
 });
