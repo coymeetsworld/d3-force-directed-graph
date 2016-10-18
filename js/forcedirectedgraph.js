dee2e80989e69ddd483b076be0bd796315265451
Coy Sanders <coymeetsworld@gmail.com>
2016-10-18 04:02:17 -0700

    DataVis: ForceDirectedGraph: Minor refactoring, cleaned up formatting.

--- js/forcedirectedgraph.js
+++ js/forcedirectedgraph.js
@@ -15,84 +15,76 @@ $(document).ready(function() {
     .force("center", d3.forceCenter(width / 2, height / 2));
 
 
-d3.json('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json', function(error, graph) {
-  if (error) throw error;
+  d3.json('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json', function(error, graph) {
+    if (error) throw error;
   
-  
-  console.log(graph);
-  simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
-  simulation.force("link")
-      .links(graph.links);
+    simulation.nodes(graph.nodes)
+              .on("tick", ticked);
+    simulation.force("link")
+              .links(graph.links);
 
-  var link = svg.selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link");
+    var link = svg.selectAll("line")
+                  .data(graph.links)
+                  .enter().append("line")
+                  .attr("class", "link");
       
+    var nodes = svg.selectAll("g.node")
+                   .data(graph.nodes).enter()
+                   .append("g")
+                   .attr("class", "node");
+  
+    nodes.data(graph.nodes).append("svg:image")
+         .attr("xlink:href",  function(d) { return "images/flags/" + d.country + ".png"; })
+         .attr("x", -10)
+         .attr("y", -10)
+         .attr("height", 32)
+         .attr("width", 32)
+         .call(d3.drag()
+                 .on("start", dragstarted)
+                 .on("drag", dragged)
+                 .on("end", dragended))
+                 .on("mouseover", function() {
 
-  var nodes = svg.selectAll("g.node").data(graph.nodes).enter()
-                        .append("g")
-                        .attr("class", "node");
-  
-  
-    
-  nodes.data(graph.nodes).append("svg:image")
-        .attr("xlink:href",  function(d) { return "images/flags/" + d.country + ".png";})
-        .attr("x", -10)
-        .attr("y", -10)
-        .attr("height", 32)
-        .attr("width", 32)
-        .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended))
-        .on("mouseover", function() {
-          var countryObj = d3.select(this).datum();
-          var country = countryObj.country;
-         	var xPos = countryObj.x;
-					var yPos = countryObj.y;
+                    var countryObj = d3.select(this).datum();
+                    var country = countryObj.country;
+         	          var xPos = countryObj.x;
+					          var yPos = countryObj.y;
           
-          svg.append('rect')
-						 .attr('class', 'tip-box')
-						 .attr('x', xPos - 30)
-						.attr('y', yPos - 30)
-						.attr('rx', 3)
-						.attr('ry', 3)
-						.attr('width', (country.length*10)+15)
-						.attr('height', 25);
+                    svg.append('rect')
+						           .attr('class', 'tip-box')
+						           .attr('x', xPos - 30)
+						           .attr('y', yPos - 30)
+						           .attr('rx', 3)
+						           .attr('ry', 3)
+						           .attr('width', (country.length*10)+15)
+						           .attr('height', 25);
 
-					svg.append('text')
-						.attr('class','tip-text')
-						.html(country)
-						.attr('x', xPos - 25)
-						.attr('y', yPos - 11);
-        }) 
-      	.on('mouseout', function(d) {
-					 svg.selectAll('.tip-box').remove();
-					 svg.selectAll('.tip-text').remove();
-			 });
+					          svg.append('text')
+						           .attr('class','tip-text')
+						           .html(country)
+						           .attr('x', xPos - 25)
+						           .attr('y', yPos - 11);
+                  }) 
+      	          .on('mouseout', function(d) {
+					          svg.selectAll('.tip-box').remove();
+					          svg.selectAll('.tip-text').remove();
+			            });
 
-
-
-  function ticked() {
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    function ticked() {
+      link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
     
+      nodes.attr("transform", function(d) { 
+        var x = Math.max(radius, Math.min(width - radius, d.x));
+        var y = Math.max(radius, Math.min(height - radius, d.y));
+        return "translate(" + x + "," + y + ")";
+        
+      });
+    }
     
-   nodes.attr("transform", function(d) { 
-       
-     var x = Math.max(radius, Math.min(width - radius, d.x));
-     var y = Math.max(radius, Math.min(height - radius, d.y));
-     return "translate(" + x + "," + y + ")"; });
-
-  }
-});
-
+  });
 
 	function dragstarted(d) {
 		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
